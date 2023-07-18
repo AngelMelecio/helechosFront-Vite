5f09d86f217bd934e5a94e4bd87364d9117ebc9b
AngelMelecio <joseangelsm50@gmail.com>
2023-07-17 21:40:56 -0600

    Produccion Context, escaneo de produccion

---
+++ src/pages/Produccion/components/ResponseModal.jsx
@@ -0,0 +1,73 @@
+import { ICONS } from "../../../constants/icons"
+
+const ResponseModal = ({ response, onClose }) => {
+  return (
+    <div className="flex grayTrans w-full h-full total-center">
+      <div className='flex flex-col h-4/5 w-3/6 rounded-xl bg-white shadow-lg p-4 modal-box'>
+        <div className='flex flex-row total-center relative'>
+          <button
+            className='normal-button total-center p-1 text-white h-8 w-8 rounded-lg absolute right-0'
+            onClick={onClose}
+          >
+            <ICONS.Check className='m-0' size='18px' />
+          </button>
+          <div className="font-semibold text-xl text-teal-700">
+            Resumen de la captura
+          </div>
+        </div>
+        <div className="py-5">
+          <table>
+            <tbody>
+              {
+                [
+                  { label: 'Empleado', atr: 'empleado' },
+                  { label: 'Departamento', atr: 'departamento' },
+                  { label: 'Fecha de captura', atr: 'fecha' },
+                ].map((column, index) =>
+                  <tr key={"ER" + index} className={(index ? "border-t" : "") + " h-7"}>
+                    <td className="text-sm font-semibold text-teal-700 px-4"> {column.label}:</td>
+                    <td className="text-base text-gray-800 w-full"> {response[column.atr]} </td>
+                  </tr>)}
+            </tbody>
+          </table>
+        </div>
+        <h3 className=" mt-2 relative h-7 border-t-2 total-center">
+              <p className="absolute -top-4 bg-white font-medium px-2 text-center italic text-teal-700">
+                Resultados
+              </p>
+        </h3>
+        <div className="overflow-y-scroll h-full">
+          <table className="customTable text-center">
+            <thead>
+              <tr>
+                <th>Modelo</th>
+                <th>Num. Etiqueta</th>
+                <th>Resultado</th>
+                <th>Detalles</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                response.registros.map((etiqueta, index) =>
+                  <tr key={'etiqueta' + index} > {
+                    ['modelo', 'numEtiqueta', 'ok', 'Detalles'].map(atr => <td>
+                      <div className="total-center">
+                        {typeof etiqueta[atr] === 'boolean' ?
+                          (etiqueta[atr] ?
+                            <ICONS.Check size="18px" color="#14b8a6"/> :
+                            <ICONS.Cancel size="18px" color="#e11d48" />)
+                          :
+                          etiqueta[atr]
+                        }
+                      </div>
+                    </td>)
+                  }
+                  </tr>)
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>)
+}
+export default ResponseModal
---
+++ src/pages/Produccion/hooks/useProduccion.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { useAuth } from "../../../context/AuthContext";
+import { useState } from "react";
+import { useContext } from "react";
+import { fetchAPI } from "../../../services/fetchApiService";
+import { entorno } from "../../../constants/entornos";
+import { set } from "lodash";
+
+const API_REGISTROS_URL = "api/registros/"
+
+const ProduccionContext = React.createContext('ProduccionContext')
+
+export function useProduccion() {
+    return useContext(ProduccionContext)
+}
+  
+
+export function ProduccionProvider({ children }) {
+
+    const { session, notify } = useAuth()
+
+    const [loading, setLoading] = useState(false)
+  
+
+
+    async function postProduccion(registros) {
+        let options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + session.access
+            },
+            body: JSON.stringify(registros)
+        }
+        try{
+            setLoading(true)
+            const response = await fetchAPI(API_REGISTROS_URL, options)
+            return response
+        }catch(e){
+            console.log('error al crear registros: ', e)
+        }finally{
+            setLoading(false)
+        }
+    }
+   
+
+    return (
+        <ProduccionContext.Provider
+            value={{
+                postProduccion,
+                loading,
+            }}
+        >
+            {children}
+        </ProduccionContext.Provider>
+    )
+}
