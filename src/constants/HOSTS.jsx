842e2780e9a2bb0828a56d0dc31d011e9128083b
César Antonio Navarro Sosa <cesaaar26@gmail.com>
2023-08-05 12:00:33 -0600

    Nueva logica en maquinas - captura de produccion

--- src/constants/HOSTS.jsx
+++ src/constants/HOSTS.jsx
@@ -1,5 +1,5 @@
-//export const WS_PREFIX="ws://localhost:8000"
-//export const API_URL = "http://localhost:8000"
+export const WS_PREFIX="ws://localhost:8000"
+export const API_URL = "http://localhost:8000"
 
-export const API_URL = "http://192.168.1.133:8000"
-export const WS_PREFIX="ws://192.168.1.133:8000"
+//export const API_URL = "http://192.168.1.133:8000"
+//export const WS_PREFIX="ws://192.168.1.133:8000"
